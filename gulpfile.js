6e555fe734b754afb5f0ded3bd3e266444ee7236
4el213@gmail.com <4el213@gmail.com>
2018-05-13 12:57:34 +0300

    yнастроил таски

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,15 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var cssmin = require("gulp-csso");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var del = require("del");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -14,9 +22,61 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(cssmin())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
+
+gulp.task("html", function() {
+  gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function() {
+  gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function() {
+  gulp.src("build/img/sprite/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function() {
+  gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/js/**/*",
+    "source/img/**/*"
+    ], {
+      base: "source"
+  })
+  .pipe(gulp.dest("build"))
+});
+
+gulp.task("clean", function() {
+  del("build");
+});
+
+gulp.task("build", function(done) {
+  run("clean", "style", "sprite", "html", done);
+});
+
+
+
 
 gulp.task("serve", ["style"], function() {
   server.init({
