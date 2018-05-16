ae5f6c7b49d1da2ec38d7540d48d2c6169eb1d5e
4el213@gmail.com <4el213@gmail.com>
2018-05-16 17:12:23 +0300

    собрал все модалки в один файл

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,12 @@
 var controlPopup = document.querySelector(".main-header__control-popup");
 var navMenu = document.querySelector(".site-navigation");
-console.log(controlPopup);
-console.log(navMenu);
 
+var buy = document.querySelectorAll (".catalog__button-buy");
+var btn = document.querySelector(".goods-week__button");
+var modal = document.querySelector(".modal");
+var overlay = document.querySelector(".modal__overlay");
+
+//nav menu
 controlPopup.classList.add("open");
 navMenu.classList.add("close");
 
@@ -10,4 +14,25 @@ controlPopup.addEventListener("click", function() {
   controlPopup.classList.toggle("open");
   controlPopup.classList.toggle("close");
   navMenu.classList.toggle("close");
+});
+
+//modal for catalog
+for (var i = 0; i < buy.length; i++) {
+  buy[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("visible");
+  });
+}
+overlay.addEventListener("click", function() {
+  modal.classList.remove("visible");
+});
+
+//modal for index
+btn.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.add("visible");
+});
+
+overlay.addEventListener("click", function() {
+  modal.classList.remove("visible");
 });
