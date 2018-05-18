53cf6f009870f30123f4fbc8a36bc48444a1a355
4el213@gmail.com <4el213@gmail.com>
2018-05-18 16:11:52 +0300

    форматированный JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 var controlPopup = document.querySelector(".main-header__control-popup");
 var navMenu = document.querySelector(".site-navigation");
 
-var buy = document.querySelectorAll (".catalog__button-buy");
+var buy = document.querySelectorAll(".catalog__button-buy");
 var btn = document.querySelector(".goods-week__button");
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".modal__overlay");
@@ -10,7 +10,7 @@ var overlay = document.querySelector(".modal__overlay");
 controlPopup.classList.add("open");
 navMenu.classList.add("close");
 
-controlPopup.addEventListener("click", function() {
+controlPopup.addEventListener("click", function () {
   controlPopup.classList.toggle("open");
   controlPopup.classList.toggle("close");
   navMenu.classList.toggle("close");
@@ -18,21 +18,21 @@ controlPopup.addEventListener("click", function() {
 
 //modal for catalog
 for (var i = 0; i < buy.length; i++) {
-  buy[i].addEventListener("click", function(evt) {
+  buy[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     modal.classList.add("visible");
   });
 }
-overlay.addEventListener("click", function() {
+overlay.addEventListener("click", function () {
   modal.classList.remove("visible");
 });
 
 //modal for index
-btn.addEventListener("click", function(evt) {
+btn.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.add("visible");
 });
 
-overlay.addEventListener("click", function() {
+overlay.addEventListener("click", function () {
   modal.classList.remove("visible");
 });
